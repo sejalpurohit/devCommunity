d8c09a1bb68f46d9f412b103783ee6116914ebfd
sejal <sejalpurohit3195@gmail.com>
2025-09-04 20:53:42 +0100

    test

--- src/models/user.js
+++ src/models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    // required: true,
+    required: true,
   },
   lastName: {
     type: String,
